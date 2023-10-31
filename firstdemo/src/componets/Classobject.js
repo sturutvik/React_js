d68a5c1a0b701b0a5369aefcd46aca105559e6af
Payal311 <payalpadhiyar311@gmail.com>
2023-10-31 11:41:11 +0530

    classobject

--- firstdemo/src/componets/Classobject.js
+++ firstdemo/src/componets/Classobject.js
@@ -3,13 +3,23 @@ import React, { Component } from 'react';
 class Classobject extends Component {
     render() {
         const arr = ["abc" ,"xyz" ,"pqr"];
-        const [a,b,c] = arr
+        const [a,b,c] = arr //sepding
+        const arr1 = [10,20,30]
+        const arr2 = [...arr1, ...arr] // marging 
         return (
             <div>
                 
             <h3> a is --{a}</h3>
             <h3> b is --{b}</h3>
             <h3> c is --{c}</h3>
+            <h3>array list</h3>
+            <ul>
+                {
+                    arr2.map((i)=>{
+                        return<li>{i}</li>
+                    })
+                }
+            </ul>
                 
             </div>
         );
