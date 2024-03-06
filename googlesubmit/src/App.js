56184e3c2dfaa6732a7d3d3463d9089b7fd70f6a
sturutvik <135806914+sturutvik@users.noreply.github.com>
2024-03-06 09:48:12 +0530

    googleouath

---
+++ googlesubmit/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Loginoauth from './component/Loginoauth';
+// import Googleauth from './component/Googleauth';
+
+function App() {
+  return (
+    <div className="App">
+         {/* <Googleauth/>
+        */}
+        <Loginoauth/>
+    </div>
+  );
+}
+
+export default App;
---
+++ googlesubmit/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';  
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+      <GoogleOAuthProvider clientId="753682475228-8ghdo9e8fbn0bqkk5sp7cpdeqnud79dl.apps.googleusercontent.com">
+    
+    <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+ 
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
